0d3a4ddfef5f81d4ff8ca3d161f335834beb3443
Luan Campos <luancamposdeveloper@gmail.com>
2025-07-29 06:31:50 -0300

    chore: remove dtos ans update app module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule, UsersModule, ConfigModule.forRoot()],
+  imports: [PrismaModule, ConfigModule.forRoot()],
 })
 export class AppModule {}
