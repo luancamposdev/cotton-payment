10850492c07d0c171418bbb61f2c49edb2be56bb
Luan Campos <luancamposdeveloper@gmail.com>
2025-07-29 06:28:02 -0300

    chore: create use case find use by email

---
+++ apps/api/src/application/user/use-case/find-user-by-email.use-case.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+
+import { UserEntity } from '@core/users/entities/user.entity';
+import { UserRepository } from '@core/users/repositories/user.repository';
+
+interface FindUserByEmailRequest {
+  email: string;
+}
+
+interface FindUserByEmailResponse {
+  user: UserEntity;
+}
+
+@Injectable()
+export class FindUserByEmail {
+  constructor(private readonly userRepository: UserRepository) {}
+  async execute(
+    request: FindUserByEmailRequest,
+  ): Promise<FindUserByEmailResponse> {
+    const { email } = request;
+
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) throw new Error('User not found');
+
+    return { user };
+  }
+}
