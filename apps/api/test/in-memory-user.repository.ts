97ef27ced6a05d8d029755bc3e2e2c66af07d64f
Luan Campos <luancamposdeveloper@gmail.com>
2025-07-29 06:28:41 -0300

    chore: create in memory repo

---
+++ apps/api/test/in-memory-user.repository.ts
@@ -0,0 +1,17 @@
+import { UserRepository } from '@core/users/repositories/user.repository';
+import { UserEntity } from '@core/users/entities/user.entity';
+
+export class InMemoryUserRepository implements UserRepository {
+  public users: UserEntity[] = [];
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async findByEmail(email: string): Promise<UserEntity | null> {
+    const user = this.users.find((user) => user.email.value === email);
+    return user ?? null;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async create(user: UserEntity): Promise<void> {
+    this.users.push(user);
+  }
+}
