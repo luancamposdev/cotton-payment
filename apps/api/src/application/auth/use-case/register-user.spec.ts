a51f63620e8d9b9400d0d5a76f7077b82065d201
Luan Campos <luancamposdeveloper@gmail.com>
2025-07-31 13:25:59 -0300

    test: create register test

---
+++ apps/api/src/application/auth/use-case/register-user.spec.ts
@@ -0,0 +1,28 @@
+import { InMemoryUserRepository } from '@test/in-memory-user.repository';
+import { RegisterUser } from '@application/auth/use-case/register-user.use-case';
+import { Password } from '@core/shared/value-objects/password';
+import { Role } from '@core/users/entities/user.entity';
+import { Name } from '@core/users/value-objects/name';
+import { Email } from '@core/shared/value-objects/email';
+import { AvatarUrl } from '@core/users/value-objects/avatar-url';
+
+describe('Register user', () => {
+  it('Should be able to register user', async () => {
+    const userRepository = new InMemoryUserRepository();
+    const registerUser = new RegisterUser(userRepository);
+
+    const password = Password.create('myPassword123');
+
+    const { user } = await registerUser.execute({
+      name: Name.create('Luan Campos'),
+      email: Email.create('luancampos@mail.com'),
+      avatarUrl: AvatarUrl.create('https://luancampos.png'),
+      password,
+      role: Role.CREATOR,
+    });
+
+    expect(user).toBeTruthy();
+    expect(user.email.value).toBe('luancampos@mail.com');
+    expect(user.name.value).toBe('Luan Campos');
+  });
+});
