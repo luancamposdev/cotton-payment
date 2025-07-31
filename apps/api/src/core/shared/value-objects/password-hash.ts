462d7832eb9b6f401695bee2f527799c0c125d9e
Luan Campos <luancamposdeveloper@gmail.com>
2025-07-31 13:25:40 -0300

    chore: adjust function compare

--- apps/api/src/core/shared/value-objects/password-hash.ts
+++ apps/api/src/core/shared/value-objects/password-hash.ts
@@ -1,33 +1,32 @@
 import * as bcrypt from 'bcrypt';
-
 import { Password } from '@core/shared/value-objects/password';
 
 export class PasswordHash {
   private readonly _hash: string;
 
+  private constructor(hash: string) {
+    this._hash = hash;
+  }
+
   public value(): string {
     return this._hash;
   }
 
+  public async compare(password: Password): Promise<boolean> {
+    return bcrypt.compare(password.value(), this._hash);
+  }
+
   static async fromPassword(password: Password): Promise<PasswordHash> {
     const saltRounds = 10;
-    const hashed: string = await bcrypt.hash(password.value(), saltRounds);
+    const hashed = await bcrypt.hash(password.value(), saltRounds);
     return new PasswordHash(hashed);
   }
 
   static fromHash(hash: string): PasswordHash {
-    if (!hash.startsWith('$2')) {
+    if (!hash || !hash.startsWith('$2')) {
       throw new Error('Invalid password hash');
     }
 
     return new PasswordHash(hash);
-  }
-
-  async compare(password: Password): Promise<boolean> {
-    return bcrypt.compare(password.value(), this._hash);
-  }
-
-  private constructor(hash: string) {
-    this._hash = hash;
   }
 }
