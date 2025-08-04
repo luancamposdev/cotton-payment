2e3ae3c33f7d33ee2cf3225549493a19c702d25e
luan <luancamposdeveloper@gmail.com>
2025-08-04 15:33:47 -0300

    chore: remove no usage imports

--- apps/api/src/auth/dto/auth-reponse.dto.ts
+++ apps/api/src/auth/dto/auth-reponse.dto.ts
@@ -1,6 +1,3 @@
-import { Name } from '@core/users/value-objects/name';
-import { Email } from '@core/shared/value-objects/email';
-import { AvatarUrl } from '@core/users/value-objects/avatar-url';
 import { Role } from '@core/users/entities/user.entity';
 
 export class AuthResponseDto {
