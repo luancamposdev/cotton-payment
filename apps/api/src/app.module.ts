d829ea70e93ea775368e31086b0292669691c5c0
luan <luancamposdeveloper@gmail.com>
2025-08-04 15:43:33 -0300

    feat(app): wire up core modules including AuthModule and PrismaModule

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
-import { CustomersModule } from './customers/customers.module';
+
+import { AuthModule } from '@/auth/auth.module';
+import { PrismaModule } from '@/infrastructure/database/prisma/prisma.module';
+import { PrismaService } from '@/infrastructure/database/prisma/prisma.service';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot(), CustomersModule],
+  imports: [PrismaModule, ConfigModule.forRoot(), AuthModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
