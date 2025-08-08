46b99925154427626f1a5882807ee93844b28dba
luan <luancamposdeveloper@gmail.com>
2025-08-08 06:40:45 -0300

    fix(auth): Importa ConfigModule para resolver dependência da JwtStrategy Adiciona o ConfigModule aos imports do AuthModule. Isso resolve o UnknownDependenciesException e permite que o ConfigService seja injetado e utilizado pela JwtStrategy.

--- apps/api/src/auth/auth.module.ts
+++ apps/api/src/auth/auth.module.ts
@@ -2,18 +2,20 @@ import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtConfigService } from '@/infrastructure/auth/jwt/jwt-config.service';
-import { AuthController } from '@/auth/auth.controller';
-import { AuthService } from '@/auth/auth.service';
+import { AuthController } from '@/infrastructure/auth/controllers/auth.controller';
+import { AuthService } from '@/infrastructure/auth/auth.service';
 import { JwtStrategy } from '@/infrastructure/auth/passport/strategies/jwt.strategy';
 import { LocalStrategy } from '@/infrastructure/auth/passport/strategies/local.strategy';
 import { UserRepository } from '@core/users/repositories/user.repository';
-import { InMemoryUserRepository } from '@test/in-memory-user.repository';
 import { RegisterUser } from '@application/auth/use-case/register-user.use-case';
 import { LoginUseCase } from '@application/auth/use-case/login.use-case';
+import { PrismaUserRepository } from '@/infrastructure/database/prisma/repositories/prisma-user.repository';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule,
+    ConfigModule,
     JwtModule.registerAsync({
       useClass: JwtConfigService,
     }),
@@ -25,7 +27,7 @@ import { LoginUseCase } from '@application/auth/use-case/login.use-case';
     LoginUseCase,
     JwtStrategy,
     LocalStrategy,
-    { provide: UserRepository, useClass: InMemoryUserRepository },
+    { provide: UserRepository, useClass: PrismaUserRepository },
   ],
   exports: [AuthService],
 })
