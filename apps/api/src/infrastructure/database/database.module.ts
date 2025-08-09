285b37b5d6f3b4bc986c2babfa0f3c8e87d3fd9f
luan <luancamposdeveloper@gmail.com>
2025-08-08 23:16:07 -0300

    chore: remove import prisma-service non used

--- apps/api/src/infrastructure/database/database.module.ts
+++ apps/api/src/infrastructure/database/database.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 
-import { PrismaService } from './prisma/prisma.service';
 import { PrismaUserRepository } from './prisma/repositories/prisma-user.repository';
 import { UserRepository } from '@core/users/repositories/user.repository';
 
