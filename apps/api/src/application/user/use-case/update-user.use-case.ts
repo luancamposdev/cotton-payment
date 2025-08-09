8924ed61e7e2bc0cd1640511d31a957623733472
luan <luancamposdeveloper@gmail.com>
2025-08-09 02:03:11 -0300

    feat: create update user

---
+++ apps/api/src/application/user/use-case/update-user.use-case.ts
@@ -0,0 +1,50 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { UserEntity } from '@core/users/entities/user.entity';
+import { UserRepository } from '@core/users/repositories/user.repository';
+import { Name } from '@core/users/value-objects/name';
+import { AvatarUrl } from '@core/users/value-objects/avatar-url';
+import { Role } from '@core/users/entities/user.entity';
+
+interface IUpdateUserRequest {
+  userId: string;
+  name?: string;
+  avatarUrl?: string;
+  role?: Role;
+}
+
+interface IUpdateUserResponse {
+  user: UserEntity;
+}
+
+@Injectable()
+export class UpdateUser {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async execute({
+    userId,
+    name,
+    avatarUrl,
+    role,
+  }: IUpdateUserRequest): Promise<IUpdateUserResponse> {
+    const user = await this.userRepository.findById(userId);
+
+    if (!user) throw new NotFoundException('Usuário não encontrado.');
+
+    if (name) {
+      user.name = Name.create(name);
+    }
+
+    if (avatarUrl) {
+      user.avatarUrl = AvatarUrl.create(avatarUrl, '');
+    }
+
+    if (role) {
+      user.role = role;
+    }
+
+    await this.userRepository.save(user);
+
+    return { user };
+  }
+}
