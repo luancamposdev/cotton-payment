925136824dc2879d3771460612a349d593e5706e
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-16 16:23:17 -0300

    feat: add in-memory Creator repository for testing

---
+++ apps/api/test/in-memory-creator.repository.ts
@@ -0,0 +1,28 @@
+import { CreatorsEntity } from '@core/creators/creators.entity';
+
+export class InMemoryCreatorRepository {
+  private creators: CreatorsEntity[] = [];
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async create(creator: CreatorsEntity): Promise<void> {
+    this.creators.push(creator);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async findById(id: string): Promise<CreatorsEntity | null> {
+    return this.creators.find((c) => c.id === id) ?? null;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async findByUserId(userId: string): Promise<CreatorsEntity | null> {
+    return this.creators.find((c) => c.userId === userId) ?? null;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async save(creator: CreatorsEntity): Promise<void> {
+    const index = this.creators.findIndex((c) => c.id === creator.id);
+    if (index !== -1) {
+      this.creators[index] = creator;
+    }
+  }
+}
