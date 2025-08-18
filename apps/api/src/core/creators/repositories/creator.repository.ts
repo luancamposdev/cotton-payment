1b805590f0ec453ab0086c116c7441dde4f198ba
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-18 00:32:47 -0300

    refactor: remove findById from CreatorRepository

--- apps/api/src/core/creators/repositories/creator.repository.ts
+++ apps/api/src/core/creators/repositories/creator.repository.ts
@@ -2,7 +2,6 @@ import { CreatorsEntity } from '@core/creators/creators.entity';
 
 export abstract class CreatorRepository {
   abstract create(creator: CreatorsEntity): Promise<void>;
-  abstract findById(id: string): Promise<CreatorsEntity | null>;
   abstract findByUserId(userId: string): Promise<CreatorsEntity | null>;
   abstract save(creator: CreatorsEntity): Promise<void>;
 }
