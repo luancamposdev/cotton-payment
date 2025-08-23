382f89276f0c554313d18a69a016ab630a6f97bb
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-23 12:44:29 -0300

    test(customer): add unit tests for FindCustomerByUserIdUseCase

---
+++ apps/api/test/unit/application/customer/use-cases/find-customer-by-user-id.use-case.spec.ts
@@ -0,0 +1,42 @@
+import { InMemoryCustomerRepository } from '@test/in-memory-customer.repository';
+import { CreateCustomerUseCase } from '@application/customer/use-case/create-customer.use-case';
+import { FindCustomerByUserIdUseCase } from '@application/customer/use-case/find-customer-by-user-id.use-case';
+
+describe('Find creator byUserId', () => {
+  it('Should return customer by userId', async () => {
+    const customerRepository = new InMemoryCustomerRepository();
+    const createCustomerUseCase = new CreateCustomerUseCase(customerRepository);
+    const findCustomerByUserIdUseCase = new FindCustomerByUserIdUseCase(
+      customerRepository,
+    );
+
+    const dto = {
+      userId: '123456',
+      defaultAddressId: 'address-1234567',
+    };
+
+    await createCustomerUseCase.execute(dto);
+
+    const { customer } = await findCustomerByUserIdUseCase.execute({
+      userId: '123456',
+    });
+
+    expect(customer).toBeTruthy();
+    expect(customer).toBeDefined();
+    expect(customer.userId).toBe('123456');
+    expect(customer.defaultAddressId).toBe('address-1234567');
+  });
+
+  it('should throw an error if no customer is found by userId', async () => {
+    const customerRepository = new InMemoryCustomerRepository();
+    const findCustomerByUserIdUseCase = new FindCustomerByUserIdUseCase(
+      customerRepository,
+    );
+
+    const userId = '123456';
+
+    await expect(
+      findCustomerByUserIdUseCase.execute({ userId }),
+    ).rejects.toThrow(`User with userId ${userId} not found`);
+  });
+});
