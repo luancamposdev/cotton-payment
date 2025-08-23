decbc7015363540473a5e8375ea03e0b7dafc91b
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-23 15:01:21 -0300

    feat(customer): add CustomerModule to app module imports

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,10 +5,17 @@ import { AuthModule } from '@/interfaces/auth/auth.module';
 import { PrismaService } from '@/infrastructure/database/prisma/prisma.service';
 import { UserModule } from '@/interfaces/users/user.module';
 import { CreatorsModule } from '@/interfaces/creators/creators.module';
+import { CustomerModule } from '@/interfaces/customer/customer.module';
 
 @Global()
 @Module({
-  imports: [ConfigModule.forRoot(), AuthModule, UserModule, CreatorsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    UserModule,
+    CreatorsModule,
+    CustomerModule,
+  ],
   providers: [PrismaService],
   exports: [PrismaService],
 })
