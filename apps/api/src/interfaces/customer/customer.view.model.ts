0a1233f777a3cfcb825a376ffbd84a22bd252f89
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-23 14:45:57 -0300

    feat(customer): add CustomerViewModel for transforming CustomerEntity to HTTP response

---
+++ apps/api/src/interfaces/customer/customer.view.model.ts
@@ -0,0 +1,15 @@
+import { CustomerEntity } from '@core/customer/entities/customer.entity';
+
+export interface CustomerView {
+  id: string;
+  defaultAddressId?: string | null;
+}
+
+export class CustomerViewModel {
+  static toHTTP(customer: CustomerEntity): CustomerView {
+    return {
+      id: customer.id,
+      defaultAddressId: customer.defaultAddressId,
+    };
+  }
+}
