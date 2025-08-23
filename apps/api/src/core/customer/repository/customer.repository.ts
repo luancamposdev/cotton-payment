e0b0aa750e9862f34e1532c068a7346dc7c43a61
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-23 14:45:47 -0300

    fix(customer): update save method parameter name for clarity

--- apps/api/src/core/customer/repository/customer.repository.ts
+++ apps/api/src/core/customer/repository/customer.repository.ts
@@ -3,5 +3,5 @@ import { CustomerEntity } from '@core/customer/entities/customer.entity';
 export abstract class CustomerRepository {
   abstract create(customer: CustomerEntity): Promise<void>;
   abstract findByUserId(userId: string): Promise<CustomerEntity | null>;
-  abstract save(creator: CustomerEntity): Promise<void>;
+  abstract save(customer: CustomerEntity): Promise<void>;
 }
