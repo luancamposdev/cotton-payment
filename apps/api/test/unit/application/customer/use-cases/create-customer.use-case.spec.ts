0e1c59150615485366e27656a3681e42c4c8fbc6
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-23 12:24:11 -0300

    test(customer): add unit tests for CreateCustomerUseCase

---
+++ apps/api/test/unit/application/customer/use-cases/create-customer.use-case.spec.ts
@@ -0,0 +1,34 @@
+import { CreateCustomerUseCase } from '@application/customer/use-case/create-customer.use-case';
+import { InMemoryCustomerRepository } from '@test/in-memory-customer.repository';
+
+describe('Creator Customer useCase', () => {
+  it('Should return the creator creator', async () => {
+    const customerRepository = new InMemoryCustomerRepository();
+    const createCustomerUseCase = new CreateCustomerUseCase(customerRepository);
+
+    const dto = {
+      userId: 'userId-123456',
+      defaultAddressId: 'address-1234567',
+    };
+
+    const { customer } = await createCustomerUseCase.execute(dto);
+
+    expect(customer).toBeTruthy();
+    expect(customer.defaultAddressId).toBe('address-1234567');
+  });
+
+  it('Do not create a duplicate creator for the same userId', async () => {
+    const customerRepository = new InMemoryCustomerRepository();
+    const createCustomerUseCase = new CreateCustomerUseCase(customerRepository);
+
+    const dto = {
+      userId: 'user-123',
+    };
+
+    await createCustomerUseCase.execute(dto);
+
+    await expect(createCustomerUseCase.execute(dto)).rejects.toThrow(
+      'Customer já cadastrado para este usuário',
+    );
+  });
+});
