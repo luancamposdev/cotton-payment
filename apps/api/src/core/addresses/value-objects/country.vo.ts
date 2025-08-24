451e5c64f99339de90e35fc25e95dd3771195bba
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-24 14:30:11 -0300

    chore(Country) add getValue function

--- apps/api/src/core/addresses/value-objects/country.vo.ts
+++ apps/api/src/core/addresses/value-objects/country.vo.ts
@@ -5,6 +5,10 @@ export class CountryVo {
     this.value = CountryVo.validateValue(value);
   }
 
+  get getValue(): string {
+    return this.value;
+  }
+
   static validateValue(value: string) {
     if (value.length !== 2) {
       throw new Error('Country must be a 2-letter ISO 3166-1 alpha-2 code.');
