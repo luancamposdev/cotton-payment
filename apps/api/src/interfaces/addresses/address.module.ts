e8fed17b9ca372d9a2e064cd091aec9416b95292
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-25 00:33:11 -0300

    feat(address) add use case of findById and FindAddressByUserId

--- apps/api/src/interfaces/addresses/address.module.ts
+++ apps/api/src/interfaces/addresses/address.module.ts
@@ -4,11 +4,15 @@ import { AddressController } from '@/interfaces/addresses/controllers/address.co
 import { CreateAddressUseCase } from '@application/address/create-address.use-case';
 import { AddressRepository } from '@core/addresses/repository/address.repository';
 import { PrismaAddressRepository } from '@infrastructure/database/prisma/repositories/prisma-address.repository';
+import { FindAddressByIdUseCase } from '@application/address/find-address-by-id.use-case';
+import { FindAddressesByUserIdUseCase } from '@application/address/find-addresses-by-user-id.use-case';
 
 @Module({
   controllers: [AddressController],
   providers: [
     CreateAddressUseCase,
+    FindAddressByIdUseCase,
+    FindAddressesByUserIdUseCase,
     {
       provide: AddressRepository,
       useClass: PrismaAddressRepository,
