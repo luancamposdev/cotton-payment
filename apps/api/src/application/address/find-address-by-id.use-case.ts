686765417421c7b957409169750c18f99f51c1d0
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-25 00:33:27 -0300

    feat(address) create find address by id use case

---
+++ apps/api/src/application/address/find-address-by-id.use-case.ts
@@ -0,0 +1,18 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { AddressRepository } from '@core/addresses/repository/address.repository';
+import { AddressEntity } from '@core/addresses/entities/address.entity';
+
+@Injectable()
+export class FindAddressByIdUseCase {
+  constructor(private readonly addressRepository: AddressRepository) {}
+
+  async execute(id: string): Promise<{ address: AddressEntity }> {
+    const address = await this.addressRepository.findById(id);
+
+    if (!address) {
+      throw new NotFoundException('Address not found');
+    }
+
+    return { address };
+  }
+}
