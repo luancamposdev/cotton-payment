3795f8ca78626bcd5eee4250f96764f2cca7e0a1
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-25 00:32:20 -0300

    feat(address) add findById and findAddressByUserId

--- apps/api/src/interfaces/addresses/controllers/address.controller.ts
+++ apps/api/src/interfaces/addresses/controllers/address.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Param } from '@nestjs/common';
 
 import { JwtAuthGuard } from '@infrastructure/auth/guards/jwt-auth.guard';
 import { CreateAddressDto } from '@/interfaces/addresses/dto/create-address.dto';
@@ -6,16 +6,38 @@ import { CurrentUser } from '@infrastructure/auth/decorators/current-user.decora
 import { UserEntity } from '@core/users/entities/user.entity';
 import { CreateAddressUseCase } from '@application/address/create-address.use-case';
 import { AddressViewModel } from '@/interfaces/addresses/address.view.model';
+import { FindAddressByIdUseCase } from '@application/address/find-address-by-id.use-case';
+import { FindAddressesByUserIdUseCase } from '@application/address/find-addresses-by-user-id.use-case';
 
 @UseGuards(JwtAuthGuard)
-@Controller('address')
+@Controller('addresses')
 export class AddressController {
-  constructor(private readonly createAddress: CreateAddressUseCase) {}
+  constructor(
+    private readonly createAddress: CreateAddressUseCase,
+    private readonly findById: FindAddressByIdUseCase,
+    private readonly findAddressesByUserId: FindAddressesByUserIdUseCase,
+  ) {}
   @Post()
   async create(@Body() dto: CreateAddressDto, @CurrentUser() user: UserEntity) {
     dto.userId = user.id;
     const { address } = await this.createAddress.execute(dto);
 
     return AddressViewModel.toHTTP(address);
+  }
+
+  @Get(':id')
+  async find(@Param() params: { id: string }) {
+    const { id } = params;
+    const { address } = await this.findById.execute(id);
+
+    return AddressViewModel.toHTTP(address);
+  }
+
+  @Get()
+  async findAll(@CurrentUser() user: UserEntity) {
+    const { id } = user;
+    const { addresses } = await this.findAddressesByUserId.execute({ id });
+
+    return addresses.map((address) => AddressViewModel.toHTTP(address));
   }
 }
