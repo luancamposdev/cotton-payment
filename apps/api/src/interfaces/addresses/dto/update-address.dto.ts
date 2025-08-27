146ae32685def477c449e98e3be3dbd062f0ec58
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-26 23:33:06 -0300

    refactor(address) extend UpdateAddressDto with PartialType of CreateAddressDto

--- apps/api/src/interfaces/addresses/dto/update-address.dto.ts
+++ apps/api/src/interfaces/addresses/dto/update-address.dto.ts
@@ -1,5 +1,9 @@
-export class UpdateAddressDto {
-  type?: string; // enum
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateAddressDto } from '@/interfaces/addresses/dto/create-address.dto';
+import { AddressType } from '@core/addresses/entities/address.entity';
+
+export class UpdateAddressDto extends PartialType(CreateAddressDto) {
+  type?: AddressType;
   street?: string;
   number?: string;
   complement?: string;
