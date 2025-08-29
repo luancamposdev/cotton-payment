64be9aba3299eba298f718e46c8f5ec1a3322c99
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-28 21:27:30 -0300

    test(address): add unit tests for DeleteAddress use case

---
+++ apps/api/src/application/address/delete-address.use-case.ts
@@ -0,0 +1,14 @@
+import { AddressRepository } from '@core/addresses/repository/address.repository';
+
+export class DeleteAddressUseCase {
+  constructor(private readonly addressRepository: AddressRepository) {}
+
+  async execute(id: string): Promise<void> {
+    const address = await this.addressRepository.findById(id);
+    if (!address) {
+      throw new Error(`Address with id ${id} not found`);
+    }
+
+    await this.addressRepository.delete(id);
+  }
+}
---
+++ apps/api/test/unit/application/addresses/use-cases/delete-address.use-case.spec.ts
@@ -0,0 +1,40 @@
+import { InMemoryAddressRepository } from '@test/in-memory-address.repository';
+import { CreateAddressUseCase } from '@application/address/create-address.use-case';
+import { DeleteAddressUseCase } from '@application/address/delete-address.use-case';
+import { AddressType } from '@core/addresses/entities/address.entity';
+
+describe('DeleteAddressUseCase', () => {
+  it('Should be able to delete an address', async () => {
+    const addressRepository = new InMemoryAddressRepository();
+    const createAddressUseCase = new CreateAddressUseCase(addressRepository);
+    const deleteAddressUseCase = new DeleteAddressUseCase(addressRepository);
+
+    const dto = {
+      userId: 'user-1',
+      type: AddressType.RESIDENTIAL,
+      street: '123 Main St',
+      district: 'Downtown',
+      city: 'Metropolis',
+      state: 'NY',
+      postalCode: '12345678',
+    };
+
+    const { address } = await createAddressUseCase.execute(dto);
+
+    await deleteAddressUseCase.execute(address.id);
+
+    expect(addressRepository.addresses).toHaveLength(0);
+    expect(addressRepository.addresses).not.toContain(address);
+  });
+
+  it('Should throw an error if the address does not exist', async () => {
+    const addressRepository = new InMemoryAddressRepository();
+    const deleteAddressUseCase = new DeleteAddressUseCase(addressRepository);
+
+    const addressId = 'non-existent-address-id';
+
+    await expect(deleteAddressUseCase.execute(addressId)).rejects.toThrow(
+      `Address with id ${addressId} not found`,
+    );
+  });
+});
