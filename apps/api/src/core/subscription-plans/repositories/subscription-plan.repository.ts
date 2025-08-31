033762af5f89b7e8f7708389847e99d69a59d68a
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-31 17:30:14 -0300

    feat(subscription): add SubscriptionPlanRepository with method definitions

---
+++ apps/api/src/core/subscription-plans/repositories/subscription-plan.repository.ts
@@ -0,0 +1,9 @@
+import { SubscriptionPlanEntity } from '@core/subscription-plans/entities/subscription-plan.entity';
+
+export abstract class SubscriptionPlanRepository {
+  abstract create(subscriptionPlan: SubscriptionPlanEntity): Promise<void>;
+  abstract findById(id: string): Promise<SubscriptionPlanEntity | null>;
+  abstract findByCreatorId(
+    creatorId: string,
+  ): Promise<SubscriptionPlanEntity[]>;
+}
