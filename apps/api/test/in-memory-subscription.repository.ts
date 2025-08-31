f933306a3451754b31733d69767d62f0fc6f5731
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-31 17:04:46 -0300

    test(subscription): add in-memory implementation of SubscriptionRepository for testing purposes

---
+++ apps/api/test/in-memory-subscription.repository.ts
@@ -0,0 +1,49 @@
+import { SubscriptionRepository } from '@core/subscription-plans/repositories/subscription.repository';
+import { SubscriptionEntity } from '@core/subscription-plans/entities/subscription.entity';
+
+export class InMemorySubscriptionRepository implements SubscriptionRepository {
+  private subscriptions: SubscriptionEntity[] = [];
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async create(subscription: SubscriptionEntity): Promise<void> {
+    this.subscriptions.push(subscription);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async findById(id: string): Promise<SubscriptionEntity | null> {
+    const subscription = this.subscriptions.find(
+      (subscription) => subscription.id === id,
+    );
+
+    return subscription ?? null;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async findByCustomerId(
+    customerId: string,
+  ): Promise<SubscriptionEntity[] | null> {
+    const subscription = this.subscriptions.filter(
+      (subscription) => subscription.customerId === customerId,
+    );
+
+    return subscription ?? null;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async save(subscription: SubscriptionEntity): Promise<void> {
+    const index = this.subscriptions.findIndex(
+      (subscription) => subscription.id === subscription.id,
+    );
+
+    if (index >= 0) {
+      this.subscriptions[index] = subscription;
+    }
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async delete(id: string): Promise<void> {
+    this.subscriptions = this.subscriptions.filter(
+      (subscription) => subscription.id !== id,
+    );
+  }
+}
