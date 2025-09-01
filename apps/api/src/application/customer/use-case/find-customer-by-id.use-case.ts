ebd4c275a6d2da28bbe16ab73df75bd5c610b6a5
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-31 23:11:33 -0300

    feat(customer): add FindCustomerByIdUseCase to retrieve customer by ID

---
+++ apps/api/src/application/customer/use-case/find-customer-by-id.use-case.ts
@@ -0,0 +1,22 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { CustomerRepository } from '@core/customer/repository/customer.repository';
+import { CustomerEntity } from '@core/customer/entities/customer.entity';
+
+interface FindCustomerByUserIdRequest {
+  id: string;
+}
+
+@Injectable()
+export class FindCustomerByIdUseCase {
+  constructor(private readonly customerRepository: CustomerRepository) {}
+  async execute({
+    id,
+  }: FindCustomerByUserIdRequest): Promise<{ customer: CustomerEntity }> {
+    const customer = await this.customerRepository.findById(id);
+
+    if (!customer) throw new NotFoundException(`Customer with ${id} not found`);
+
+    return { customer };
+  }
+}
