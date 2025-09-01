b5421a5da30378b4ab01d308f010bc688fb85c9e
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-01 00:10:13 -0300

    feat(subscription): add FindSubscriptionPlanUseCase for fetching subscription plans by ID

---
+++ apps/api/src/application/subscription-pan/find-subscription-plan.use-case.ts
@@ -0,0 +1,32 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { SubscriptionPlanEntity } from '@core/subscription-plans/entities/subscription-plan.entity';
+import { SubscriptionPlanRepository } from '@core/subscription-plans/repositories/subscription-plan.repository';
+
+interface IFindSubscriptionPlanRequest {
+  id: string;
+}
+
+interface IFindSubscriptionPlanResponse {
+  subscriptionPlan: SubscriptionPlanEntity;
+}
+
+@Injectable()
+export class FindSubscriptionPlanUseCase {
+  constructor(
+    private readonly subscriptionPlanRepository: SubscriptionPlanRepository,
+  ) {}
+
+  async execute(
+    request: IFindSubscriptionPlanRequest,
+  ): Promise<IFindSubscriptionPlanResponse> {
+    const { id } = request;
+
+    const subscriptionPlan = await this.subscriptionPlanRepository.findById(id);
+
+    if (!subscriptionPlan)
+      throw new NotFoundException(`Subscription plan with id ${id} not found`);
+
+    return { subscriptionPlan };
+  }
+}
