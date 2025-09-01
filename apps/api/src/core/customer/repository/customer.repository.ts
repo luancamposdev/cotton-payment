50c8d1b3962395540792b0e41dd25f81ac29729a
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-31 23:12:44 -0300

    feat(customer): add findById method to CustomerRepository

--- apps/api/src/core/customer/repository/customer.repository.ts
+++ apps/api/src/core/customer/repository/customer.repository.ts
@@ -2,6 +2,7 @@ import { CustomerEntity } from '@core/customer/entities/customer.entity';
 
 export abstract class CustomerRepository {
   abstract create(customer: CustomerEntity): Promise<void>;
+  abstract findById(id: string): Promise<CustomerEntity | null>;
   abstract findByUserId(userId: string): Promise<CustomerEntity | null>;
   abstract save(customer: CustomerEntity): Promise<void>;
 }
