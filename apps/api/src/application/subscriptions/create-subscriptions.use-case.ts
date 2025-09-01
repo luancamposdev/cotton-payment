1cf9087a1b7e0b862a9f74a88e07e3676de4be3c
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-31 21:01:30 -0300

    feat(subscription): add CreateSubscriptionsUseCase for creating subscriptions

---
+++ apps/api/src/application/subscriptions/create-subscriptions.use-case.ts
@@ -0,0 +1,57 @@
+import { NotFoundException } from '@nestjs/common';
+
+import { SubscriptionRepository } from '@core/subscriptions/repositories/subscription.repository';
+import { SubscriptionEntity } from '@core/subscriptions/entities/subscription.entity';
+import { CustomerRepository } from '@core/customer/repository/customer.repository';
+import { SubscriptionPlanRepository } from '@core/subscription-plans/repositories/subscription-plan.repository';
+import { SubscriptionStatusVO } from '@core/subscriptions/value-objects/subscription-status.vo';
+
+interface ICreateSubscriptionRequest {
+  customerId: string;
+  planId: string;
+}
+
+interface ICreateSubscriptionResponse {
+  subscription: SubscriptionEntity;
+}
+
+export class CreateSubscriptionsUseCase {
+  constructor(
+    private readonly subscriptionsRepository: SubscriptionRepository,
+    private readonly customerRepository: CustomerRepository,
+    private readonly subscriptionPlanRepository: SubscriptionPlanRepository,
+  ) {}
+
+  async execute(
+    request: ICreateSubscriptionRequest,
+  ): Promise<ICreateSubscriptionResponse> {
+    const { customerId, planId } = request;
+
+    const customer = await this.customerRepository.findByUserId(customerId);
+
+    if (!customer) {
+      throw new NotFoundException('Customer not found');
+    }
+
+    const plan = await this.subscriptionPlanRepository.findByCreatorId(planId);
+
+    if (!plan) {
+      throw new NotFoundException('Subscription plan not found');
+    }
+
+    const subscription = new SubscriptionEntity({
+      customerId: customerId,
+      planId: planId,
+      status: new SubscriptionStatusVO('PENDING'),
+      startDate: new Date(),
+      endDate: null,
+      renewalAt: null,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+
+    await this.subscriptionsRepository.create(subscription);
+
+    return { subscription };
+  }
+}
