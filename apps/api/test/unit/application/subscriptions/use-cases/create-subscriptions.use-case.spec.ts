04e09d9710ae2b67333b281c0ec090d39cdd496c
Luan Campos <luancamposdeveloper@gmail.com>
2025-08-31 23:52:54 -0300

    test(subscription): refactor tests to use cases for customer and subscription plan creation

--- apps/api/test/unit/application/subscriptions/use-cases/create-subscriptions.use-case.spec.ts
+++ apps/api/test/unit/application/subscriptions/use-cases/create-subscriptions.use-case.spec.ts
@@ -3,14 +3,9 @@ import { CreateSubscriptionUseCase } from '@application/subscriptions/create-sub
 import { InMemoryCustomerRepository } from '@test/in-memory-customer.repository';
 import { InMemorySubscriptionPlanRepository } from '@test/in-memory-subscription-plan.repository';
 
-import { CustomerEntity } from '@core/customer/entities/customer.entity';
-import { SubscriptionPlanEntity } from '@core/subscription-plans/entities/subscription-plan.entity';
-
-import { SubscriptionNameVo } from '@core/subscription-plans/value-objects/subscription-plan/subscription-name.vo';
-import { PriceVO } from '@core/subscription-plans/value-objects/subscription-plan/price.vo';
-import { CurrencyVO } from '@core/subscription-plans/value-objects/subscription-plan/currency.vo';
-import { BillingIntervalVO } from '@core/subscription-plans/value-objects/subscription-plan/billing-interval.vo';
 import { SubscriptionEntity } from '@core/subscriptions/entities/subscription.entity';
+import { CreateSubscriptionPlanUseCase } from '@application/subscription-pan/create-subscription-plan.use-case';
+import { CreateCustomerUseCase } from '@application/customer/use-case/create-customer.use-case';
 
 describe('CreateSubscriptionsUseCase', () => {
   it('should create a subscription successfully', async () => {
@@ -23,44 +18,34 @@ describe('CreateSubscriptionsUseCase', () => {
       customerRepository,
       subscriptionPlanRepository,
     );
-
-    await customerRepository.create(
-      new CustomerEntity(
-        {
-          userId: 'user-123',
-          defaultAddressId: 'address-id',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        'valid-customer',
-      ),
+    const createCustomerUseCase = new CreateCustomerUseCase(customerRepository);
+    const createSubscriptionPlanUseCase = new CreateSubscriptionPlanUseCase(
+      subscriptionPlanRepository,
     );
 
-    await subscriptionPlanRepository.create(
-      new SubscriptionPlanEntity(
-        {
-          creatorId: 'creator-456',
-          name: new SubscriptionNameVo('Basic Plan'),
-          price: new PriceVO(999),
-          currency: new CurrencyVO('BRL'),
-          billingInterval: new BillingIntervalVO('MONTHLY'),
-          description: null,
-          trialDays: null,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        'valid-plan',
-      ),
-    );
+    const { customer } = await createCustomerUseCase.execute({
+      userId: 'user-123',
+      defaultAddressId: 'address-id',
+    });
+
+    const { subscriptionPlan } = await createSubscriptionPlanUseCase.execute({
+      creatorId: 'creator-456',
+      name: 'Basic Plan',
+      price: 999,
+      currency: 'BRL',
+      billingInterval: 'MONTHLY',
+      description: null,
+      trialDays: null,
+    });
 
     const result = await createSubscriptionUseCase.execute({
-      customerId: 'valid-customer',
-      planId: 'valid-plan',
+      customerId: customer.id,
+      planId: subscriptionPlan.id,
     });
 
     expect(result.subscription).toBeInstanceOf(SubscriptionEntity);
-    expect(result.subscription.customerId).toBe('valid-customer');
-    expect(result.subscription.planId).toBe('valid-plan');
+    expect(result.subscription.customerId).toBe(customer.id);
+    expect(result.subscription.planId).toBe(subscriptionPlan.id);
     expect(result.subscription.status.value).toBe('PENDING');
 
     const persisted = await subscriptionRepository.findById(
@@ -101,21 +86,16 @@ describe('CreateSubscriptionsUseCase', () => {
       subscriptionPlanRepository,
     );
 
-    await customerRepository.create(
-      new CustomerEntity(
-        {
-          userId: 'user-123',
-          defaultAddressId: 'address-123',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        'valid-customer',
-      ),
-    );
+    const createCustomerUseCase = new CreateCustomerUseCase(customerRepository);
+
+    const { customer } = await createCustomerUseCase.execute({
+      userId: 'user-123',
+      defaultAddressId: 'address-id',
+    });
 
     await expect(
       createSubscriptionUseCase.execute({
-        customerId: 'valid-customer',
+        customerId: customer.id,
         planId: 'invalid',
       }),
     ).rejects.toThrow('Subscription plan not found');
