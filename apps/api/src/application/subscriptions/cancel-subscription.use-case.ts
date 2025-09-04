38b8a5e3c272d5cc9928f877fdc7bf9a60c2508f
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-04 00:26:40 -0300

    feat(subscription): add CancelSubscription use case to handle subscription cancellations

---
+++ apps/api/src/application/subscriptions/cancel-subscription.use-case.ts
@@ -0,0 +1,31 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { SubscriptionRepository } from '@core/subscriptions/repositories/subscription.repository';
+import { SubscriptionEntity } from '@core/subscriptions/entities/subscription.entity';
+import { SubscriptionStatusVO } from '@core/subscriptions/value-objects/subscription-status.vo';
+
+interface ICancelSubscriptionResponse {
+  subscription: SubscriptionEntity;
+}
+
+@Injectable()
+export class CancelSubscriptionUseCase {
+  constructor(
+    private readonly subscriptionRepository: SubscriptionRepository,
+  ) {}
+
+  async execute(subscriptionId: string): Promise<ICancelSubscriptionResponse> {
+    const subscription =
+      await this.subscriptionRepository.findById(subscriptionId);
+
+    if (!subscription) {
+      throw new NotFoundException('Assinatura não encontrada.');
+    }
+
+    subscription.status = new SubscriptionStatusVO('CANCELLED');
+    subscription.endDate = new Date();
+    await this.subscriptionRepository.save(subscription);
+
+    return { subscription };
+  }
+}
