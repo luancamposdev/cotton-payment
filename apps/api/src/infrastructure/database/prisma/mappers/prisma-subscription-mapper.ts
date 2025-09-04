6cc241f025859e28fb434be9b6448fd2ff8a1cad
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-04 01:47:35 -0300

    feat(subscription): enhance PrismaSubscriptionMapper to support subscription creation with optional plan

--- apps/api/src/infrastructure/database/prisma/mappers/prisma-subscription-mapper.ts
+++ apps/api/src/infrastructure/database/prisma/mappers/prisma-subscription-mapper.ts
@@ -1,21 +1,30 @@
-// src/infra/database/prisma/mappers/prisma-subscription-mapper.ts
 import {
   Subscription as PrismaSubscription,
   SubscriptionStatus,
 } from '@prisma/client';
 import { SubscriptionEntity } from '@core/subscriptions/entities/subscription.entity';
 import { SubscriptionStatusVO } from '@core/subscriptions/value-objects/subscription-status.vo';
+import { SubscriptionPlanEntity } from '@core/subscription-plans/entities/subscription-plan.entity';
 
 export class PrismaSubscriptionMapper {
-  static toDomain(raw: PrismaSubscription): SubscriptionEntity {
+  static toDomain(
+    raw: PrismaSubscription,
+    plan?: SubscriptionPlanEntity,
+  ): SubscriptionEntity {
+    // Criamos a assinatura usando o createFromPlan, se tivermos o plano disponível
+    if (plan) {
+      return SubscriptionEntity.createFromPlan(raw.customerId, plan);
+    }
+
     return new SubscriptionEntity(
       {
         customerId: raw.customerId,
         planId: raw.planId,
         status: new SubscriptionStatusVO(raw.status),
         startDate: raw.startDate,
-        endDate: raw.endDate,
-        renewalAt: raw.renewalAt,
+        trialEndsAt: raw.trialEndsAt ?? null,
+        endDate: raw.endDate ?? null,
+        renewalAt: raw.renewalAt ?? null,
         createdAt: raw.createdAt,
         updatedAt: raw.updatedAt,
       },
@@ -30,6 +39,7 @@ export class PrismaSubscriptionMapper {
       planId: subscription.planId,
       status: subscription.status.value as SubscriptionStatus,
       startDate: subscription.startDate,
+      trialEndsAt: subscription.trialEndsAt,
       endDate: subscription.endDate,
       renewalAt: subscription.renewalAt,
       createdAt: subscription.createdAt,
