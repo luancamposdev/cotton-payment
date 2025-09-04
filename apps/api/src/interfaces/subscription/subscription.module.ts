f503d3dc5be005e0fb48aea83a9a798e91f0ce11
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-04 00:32:58 -0300

    feat(subscription): register CancelSubscription use case in SubscriptionModule

--- apps/api/src/interfaces/subscription/subscription.module.ts
+++ apps/api/src/interfaces/subscription/subscription.module.ts
@@ -3,17 +3,16 @@ import { Module } from '@nestjs/common';
 import { SubscriptionController } from '@/interfaces/subscription/controllers/subscription.controller';
 import { CreateSubscriptionUseCase } from '@application/subscriptions/create-subscriptions.use-case';
 
-// Repositories abstratos
 import { SubscriptionRepository } from '@core/subscriptions/repositories/subscription.repository';
 import { CustomerRepository } from '@core/customer/repository/customer.repository';
 import { SubscriptionPlanRepository } from '@core/subscription-plans/repositories/subscription-plan.repository';
 
-// Prisma implementations
 import { PrismaSubscriptionRepository } from '@infrastructure/database/prisma/repositories/prisma-subscription.repository';
 import { PrismaCustomerRepository } from '@infrastructure/database/prisma/repositories/prisma-customer.repository';
 import { PrismaSubscriptionPlanRepository } from '@infrastructure/database/prisma/repositories/prisma-subscription-plan.repository';
 import { FindSubscriptionByCustomerUseCase } from '@application/subscriptions/find-subscription-by-customer.use-case';
 import { UpdateSubscriptionUseCase } from '@application/subscriptions/update-subscription.use-case';
+import { CancelSubscriptionUseCase } from '@application/subscriptions/cancel-subscription.use-case';
 
 @Module({
   controllers: [SubscriptionController],
@@ -21,6 +20,7 @@ import { UpdateSubscriptionUseCase } from '@application/subscriptions/update-sub
     CreateSubscriptionUseCase,
     FindSubscriptionByCustomerUseCase,
     UpdateSubscriptionUseCase,
+    CancelSubscriptionUseCase,
     {
       provide: SubscriptionRepository,
       useClass: PrismaSubscriptionRepository,
