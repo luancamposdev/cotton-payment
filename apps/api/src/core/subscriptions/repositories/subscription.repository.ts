6d93374126b2fc8433f6d7acbbdd80d127b002a8
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-04 23:13:50 -0300

    feat(subscription): add findAllActiveOrTrial method to subscription repository interface

--- apps/api/src/core/subscriptions/repositories/subscription.repository.ts
+++ apps/api/src/core/subscriptions/repositories/subscription.repository.ts
@@ -5,4 +5,5 @@ export abstract class SubscriptionRepository {
   abstract findById(id: string): Promise<SubscriptionEntity | null>;
   abstract findByCustomerId(customerId: string): Promise<SubscriptionEntity[]>;
   abstract save(subscription: SubscriptionEntity): Promise<void>;
+  abstract findAllActiveOrTrial(): Promise<SubscriptionEntity[]>;
 }
