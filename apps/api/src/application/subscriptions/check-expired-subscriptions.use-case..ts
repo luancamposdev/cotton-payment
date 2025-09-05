4ca01f7b508c414c820217a81ae5299d83c30c93
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-04 23:14:24 -0300

    feat(subscription): add use case to check and update expired subscriptions

---
+++ apps/api/src/application/subscriptions/check-expired-subscriptions.use-case..ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { Cron } from '@nestjs/schedule';
+
+import { SubscriptionRepository } from '@/core/subscriptions/repositories/subscription.repository';
+import { SubscriptionStatusService } from '@/core/subscriptions/services/subscription-status.service';
+
+@Injectable()
+export class CheckExpiredSubscriptionsUseCase {
+  constructor(
+    private readonly subscriptionRepository: SubscriptionRepository,
+  ) {}
+
+  @Cron('0 * * * *') // roda a cada hora
+  async handle() {
+    const subs = await this.subscriptionRepository.findAllActiveOrTrial();
+    const now = new Date();
+
+    for (const sub of subs) {
+      const updated = SubscriptionStatusService.recalculate(sub, now);
+      await this.subscriptionRepository.save(updated);
+    }
+  }
+}
