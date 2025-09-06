89729dcee911047d7a6dbe982f9c488a708ee97f
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-05 22:47:07 -0300

    feat(api): add endpoint to retrieve payment method by ID
    - Implements `findById` method in `PaymentMethodController`.
    - Uses `FindPaymentMethodByIdUseCase` for fetching payment methods.
    - Adds necessary imports and dependency injection.

--- apps/api/src/interfaces/payments/controllers/payment-method.controller.ts
+++ apps/api/src/interfaces/payments/controllers/payment-method.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, UseGuards } from '@nestjs/common';
 
 import { JwtAuthGuard } from '@infrastructure/auth/guards/jwt-auth.guard';
 import { RolesGuard } from '@infrastructure/common/guards/roles.guard';
@@ -8,12 +8,14 @@ import { Role } from '@core/users/entities/user.entity';
 import { CreatePaymentMethodUseCase } from '@application/payments/use-cases/create-payment-method.use-case';
 import { CreatePaymentMethodDto } from '@/interfaces/payments/dto/create-payment-method.dto';
 import { PaymentMethodViewModel } from '@/interfaces/payments/payment-method.view-model';
+import { FindPaymentMethodByIdUseCase } from '@application/payments/use-cases/find-payment-method-by-id.use-case';
 
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('payment-method')
 export class PaymentMethodController {
   constructor(
     private readonly createPaymentMethodUseCase: CreatePaymentMethodUseCase,
+    private readonly findPaymentMethodByIdUseCase: FindPaymentMethodByIdUseCase,
   ) {}
 
   @Post()
@@ -21,6 +23,15 @@ export class PaymentMethodController {
   async create(@Body() dto: CreatePaymentMethodDto) {
     const { paymentMethod } =
       await this.createPaymentMethodUseCase.execute(dto);
+
+    return PaymentMethodViewModel.toHTTP(paymentMethod);
+  }
+
+  @Get(':id')
+  @Roles(Role.CUSTOMER)
+  async findById(@Param('id') id: string) {
+    const { paymentMethod } =
+      await this.findPaymentMethodByIdUseCase.execute(id);
 
     return PaymentMethodViewModel.toHTTP(paymentMethod);
   }
