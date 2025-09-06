8f94a3daf0444eb8c6658855e8c46f854c7e2590
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-05 22:48:34 -0300

    feat(api): add FindPaymentMethodByIdUseCase to PaymentMethodModule
    - Registers `FindPaymentMethodByIdUseCase` in `PaymentMethodModule` providers.

--- apps/api/src/interfaces/payments/payment-method.module.ts
+++ apps/api/src/interfaces/payments/payment-method.module.ts
@@ -6,11 +6,13 @@ import { PrismaPaymentMethodRepository } from '@infrastructure/database/prisma/r
 import { CreatePaymentMethodUseCase } from '@application/payments/use-cases/create-payment-method.use-case';
 
 import { PaymentMethodController } from '@/interfaces/payments/controllers/payment-method.controller';
+import { FindPaymentMethodByIdUseCase } from '@application/payments/use-cases/find-payment-method-by-id.use-case';
 
 @Module({
   controllers: [PaymentMethodController],
   providers: [
     CreatePaymentMethodUseCase,
+    FindPaymentMethodByIdUseCase,
     {
       provide: PaymentMethodRepository,
       useClass: PrismaPaymentMethodRepository,
