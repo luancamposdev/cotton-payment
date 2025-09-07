3f7585b7f4ed4e329eff628215fd8051bb400a44
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-07 16:17:48 -0300

    feat(order): extend `OrderController` with update, delete, and customer-based retrieval
    - Added PATCH, DELETE, and customer-specific GET endpoints.
    - Integrated new use cases for updating, deleting, and retrieving orders by customer ID.

--- apps/api/src/interfaces/order/controllers/order.controller.ts
+++ apps/api/src/interfaces/order/controllers/order.controller.ts
@@ -1,4 +1,15 @@
-import { Body, Controller, Post, UseGuards, Param, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  Param,
+  Get,
+  Delete,
+  HttpCode,
+  HttpStatus,
+  Patch,
+} from '@nestjs/common';
 
 import { JwtAuthGuard } from '@infrastructure/auth/guards/jwt-auth.guard';
 import { RolesGuard } from '@infrastructure/common/guards/roles.guard';
@@ -10,8 +21,11 @@ import { CreateOrderUseCase } from '@application/order/use-cases/create-order.us
 import { CreateOrderDTO } from '@/interfaces/order/dto/create-order.dto';
 
 import { OrderViewModel } from '@/interfaces/order/order.view.model';
-import { PaymentMethodViewModel } from '@/interfaces/payments/payment-method.view-model';
 import { FindOrderByIDUseCase } from '@application/order/use-cases/find-order-by-id.use-case';
+import { FindOrderByCustomerIdUseCase } from '@application/order/use-cases/find-order-by-customer-id.use-case';
+import { DeleteOrderUseCase } from '@application/order/use-cases/delete-order.use-case';
+import { UpdateOrderUseCase } from '@application/order/use-cases/update-order.use-case';
+import { UpdateOrderDTO } from '@/interfaces/order/dto/update-order.dto';
 
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('orders')
@@ -19,10 +33,13 @@ export class OrderController {
   constructor(
     private readonly createOrderUseCase: CreateOrderUseCase,
     private readonly findOrderByIdUseCase: FindOrderByIDUseCase,
+    private readonly findOrderByCustomerId: FindOrderByCustomerIdUseCase,
+    private readonly updateOrderUseCase: UpdateOrderUseCase,
+    private readonly deleteOrderUseCase: DeleteOrderUseCase,
   ) {}
 
   @Post(':id')
-  @Roles(Role.CUSTOMER, Role.ADMIN)
+  @Roles(Role.CUSTOMER)
   async create(@Body() dto: CreateOrderDTO, @Param('id') id: string) {
     const { order } = await this.createOrderUseCase.execute({
       ...dto,
@@ -38,5 +55,29 @@ export class OrderController {
     const { order } = await this.findOrderByIdUseCase.execute(id);
 
     return OrderViewModel.toHTTP(order);
+  }
+
+  @Get('customer/:customerId')
+  @Roles(Role.CUSTOMER, Role.ADMIN)
+  async findByCustomerId(@Param('customerId') customerId: string) {
+    const { orders } = await this.findOrderByCustomerId.execute(customerId);
+
+    return orders.map((order) => OrderViewModel.toHTTP(order));
+  }
+
+  @Roles(Role.CUSTOMER, Role.ADMIN)
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() dto: UpdateOrderDTO) {
+    const { order } = await this.updateOrderUseCase.execute(id, dto);
+
+    return OrderViewModel.toHTTP(order);
+  }
+
+  @Roles(Role.CUSTOMER, Role.ADMIN)
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  async delete(@Param('id') id: string) {
+    await this.deleteOrderUseCase.excute(id);
+    return { message: 'Pedido removido com sucesso.' };
   }
 }
