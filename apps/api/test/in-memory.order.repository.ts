1c0a4db7abfa3187a170c0c39cee8e96e8241332
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-06 23:56:14 -0300

    test(order): add in-memory implementation of `OrderRepository` for testing
    - Created `OrderRepositoryInMemory` to simulate `OrderRepository` behavior for unit tests.
    - Included support for `findById`, `findAll`, `save`, and `delete` methods.

---
+++ apps/api/test/in-memory.order.repository.ts
@@ -0,0 +1,25 @@
+import { OrderRepository } from '@core/Order/repository/order.repository';
+import { OrderEntity } from '@core/Order/entities/order.entity';
+
+export class OrderRepositoryInMemory implements OrderRepository {
+  private orders: Map<string, OrderEntity> = new Map();
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async findById(id: string): Promise<OrderEntity | null> {
+    return this.orders.get(id) ?? null;
+  }
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async findAll(): Promise<OrderEntity[]> {
+    return Array.from(this.orders.values());
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async save(order: OrderEntity): Promise<void> {
+    this.orders.set(order.id, order);
+  }
+
+  // eslint-disable-next-line @typescript-eslint/require-await
+  async delete(id: string): Promise<void> {
+    this.orders.delete(id);
+  }
+}
