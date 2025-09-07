1cfb1420cecd31174b6df080dc634d86259d5afa
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-07 15:17:54 -0300

    feat(order): validate customer existence in `CreateOrderUseCase`
    - Added `CustomerRepository` dependency to check if the customer exists.
    - Incorporated `NotFoundException` for non-existent customers during order creation.

--- apps/api/src/application/order/use-cases/create-order.use-case.ts
+++ apps/api/src/application/order/use-cases/create-order.use-case.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { OrderRepository } from '@core/Order/repository/order.repository';
 import { OrderEntity } from '@core/Order/entities/order.entity';
@@ -7,10 +7,11 @@ import { CustomerId } from '@core/payments/value-objects/customer-id.vo';
 import { AmountVO } from '@core/Order/value-objects/amount.vo';
 import { CurrencyVO } from '@core/Order/value-objects/currency.vo';
 import { PaymentStatusVO } from '@core/Order/value-objects/payment-status.vo';
+import { CustomerRepository } from '@core/customer/repository/customer.repository';
 
 interface CreateOrderRequest {
   customerId: string;
-  creatorId: string | null;
+  creatorId: string;
   amount: number; // valor em centavos
   currency: string; // 'BRL', 'USD', etc
   description: string | null;
@@ -18,13 +19,25 @@ interface CreateOrderRequest {
 
 @Injectable()
 export class CreateOrderUseCase {
-  constructor(private readonly orderRepository: OrderRepository) {}
+  constructor(
+    private readonly orderRepository: OrderRepository,
+    private readonly customerRepository: CustomerRepository,
+  ) {}
 
   async execute(request: CreateOrderRequest): Promise<{ order: OrderEntity }> {
     const customerIdVO = new CustomerId(request.customerId);
     const amountVO = new AmountVO(request.amount);
     const currencyVO = CurrencyVO.create(request.currency);
     const statusVO = new PaymentStatusVO('PENDING');
+
+    const customerExists = await this.customerRepository.findById(
+      request.customerId,
+    );
+    if (!customerExists) {
+      throw new NotFoundException(
+        `Cliente ${request.customerId} não encontrado`,
+      );
+    }
 
     const order = new OrderEntity({
       customerId: customerIdVO,
