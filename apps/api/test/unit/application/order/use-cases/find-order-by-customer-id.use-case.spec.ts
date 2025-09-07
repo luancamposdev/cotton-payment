9cc75fe6f87f47b29103fc9a0e9e08296ecba2e3
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-07 10:18:52 -0300

    test(order): add unit tests for `FindOrderByCustomerIdUseCase`
    - Verified retrieval of orders by customer ID with matching data.
    - Ensured an empty array is returned for non-existent customer IDs.

---
+++ apps/api/test/unit/application/order/use-cases/find-order-by-customer-id.use-case.spec.ts
@@ -0,0 +1,52 @@
+import { InMemoryOrderRepository } from '@test/in-memory.order.repository';
+import { CreateOrderUseCase } from '@application/order/use-cases/create-order.use-case';
+import { FindOrderByCustomerIdUseCase } from '@application/order/use-cases/find-order-by-customer-id.use-case';
+import { OrderRepository } from '@core/Order/repository/order.repository';
+
+describe('FindOrderByCustomerIdUseCase', () => {
+  let orderRepository: OrderRepository;
+  let createOrderUseCase: CreateOrderUseCase;
+  let findOrderByCustomerIdUseCase: FindOrderByCustomerIdUseCase;
+
+  beforeEach(() => {
+    orderRepository = new InMemoryOrderRepository();
+    createOrderUseCase = new CreateOrderUseCase(orderRepository);
+    findOrderByCustomerIdUseCase = new FindOrderByCustomerIdUseCase(
+      orderRepository,
+    );
+  });
+
+  it('Should return orders by customer id', async () => {
+    const orderData = {
+      customerId: 'customer-123',
+      creatorId: 'creator-456',
+      amount: 2000,
+      currency: 'BRL',
+      description: 'Pedido de teste',
+    };
+
+    await createOrderUseCase.execute(orderData);
+    await createOrderUseCase.execute(orderData);
+
+    const { orders } = await findOrderByCustomerIdUseCase.execute(
+      orderData.customerId,
+    );
+
+    expect(orders).toHaveLength(2);
+    orders.forEach((order) => {
+      expect(order.customerId.value).toBe(orderData.customerId);
+      expect(order.amount.value).toBe(orderData.amount);
+      expect(order.currency.value.toString()).toBe(orderData.currency);
+      expect(order.description).toBe(orderData.description);
+    });
+  });
+
+  it('Should return empty array when customer has no orders', async () => {
+    const { orders } = await findOrderByCustomerIdUseCase.execute(
+      'non-existent-customer',
+    );
+
+    expect(orders).toHaveLength(0);
+    expect(orders).toEqual([]);
+  });
+});
