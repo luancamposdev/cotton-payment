1d0974a61203755527b6a6523220d6f2bdf87cee
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-07 15:18:46 -0300

    feat(order): add `PrismaOrderMapper` for entity-to-DB transformations
    - Implemented `PrismaOrderMapper` with `toPrisma` and `toDomain` methods to map between `OrderEntity` and Prisma models.

---
+++ apps/api/src/infrastructure/database/prisma/mappers/prisma-order.mapper.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Order as RawOrder, PayoutStatus } from '@prisma/client';
+
+import { OrderEntity } from '@core/Order/entities/order.entity';
+import { AmountVO } from '@core/Order/value-objects/amount.vo';
+import { CurrencyVO } from '@core/Order/value-objects/currency.vo';
+import { PaymentStatusVO } from '@core/Order/value-objects/payment-status.vo';
+import { CustomerId } from '@core/payments/value-objects/customer-id.vo';
+
+@Injectable()
+export class PrismaOrderMapper {
+  static toPrisma(order: OrderEntity) {
+    return {
+      id: order.id,
+      customerId: order.customerId.value,
+      creatorId: order.creatorId,
+      amountCents: order.amount.value,
+      currency: order.currency.value,
+      description: order.description,
+      status: order.status.value as PayoutStatus,
+      createdAt: order.createdAt,
+      updatedAt: order.updatedAt,
+    };
+  }
+
+  static toDomain(raw: RawOrder): OrderEntity {
+    return new OrderEntity(
+      {
+        customerId: new CustomerId(raw.customerId),
+        creatorId: raw.creatorId,
+        amount: new AmountVO(raw.amountCents),
+        currency: CurrencyVO.create(raw.currency),
+        description: raw.description,
+        status: PaymentStatusVO.create(raw.status),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      raw.id,
+    );
+  }
+}
