14500477efaaddf31b87c57e50e505f3da4a5605
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-07 15:25:28 -0300

    feat(order): add `FindOrderByIDUseCase` to `OrderModule` providers
    - Registered `FindOrderByIDUseCase` as a provider in `OrderModule` for order retrieval logic.

--- apps/api/src/interfaces/order/order.module.ts
+++ apps/api/src/interfaces/order/order.module.ts
@@ -6,12 +6,14 @@ import { OrderController } from '@/interfaces/order/controllers/order.controller
 import { OrderRepository } from '@core/Order/repository/order.repository';
 import { PrismaOrderRepository } from '@infrastructure/database/prisma/repositories/prisma-order.repository';
 import { CustomerModule } from '@/interfaces/customer/customer.module';
+import { FindOrderByIDUseCase } from '@application/order/use-cases/find-order-by-id.use-case';
 
 @Module({
   imports: [CustomerModule],
   controllers: [OrderController],
   providers: [
     CreateOrderUseCase,
+    FindOrderByIDUseCase,
     {
       provide: OrderRepository,
       useClass: PrismaOrderRepository,
