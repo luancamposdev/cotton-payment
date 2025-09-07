a85ebc7df5f77817935be56dd28bfea5dfca3da6
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-07 00:10:24 -0300

    feat(order): implement `CreateOrderUseCase` for creating new orders
    - Added `CreateOrderUseCase` to handle order creation with validation logic.
    - Integrated value objects for `Amount`, `Currency`, `PaymentStatus`, and `CustomerId`.
    - Utilized `OrderRepository` for persisting new orders.

---
+++ apps/api/src/application/order/use-cases/create-order.use-case.ts
@@ -0,0 +1,39 @@
+import { OrderRepository } from '@core/Order/repository/order.repository';
+import { OrderEntity } from '@core/Order/entities/order.entity';
+
+import { CustomerId } from '@core/payments/value-objects/customer-id.vo';
+import { AmountVO } from '@core/Order/value-objects/amount.vo';
+import { CurrencyVO } from '@core/Order/value-objects/currency.vo';
+import { PaymentStatusVO } from '@core/Order/value-objects/payment-status.vo';
+
+interface CreateOrderRequest {
+  customerId: string;
+  creatorId: string | null;
+  amount: number; // valor em centavos
+  currency: string; // 'BRL', 'USD', etc
+  description: string | null;
+}
+
+export class CreateOrderUseCase {
+  constructor(private readonly orderRepository: OrderRepository) {}
+
+  async execute(request: CreateOrderRequest): Promise<{ order: OrderEntity }> {
+    const customerIdVO = new CustomerId(request.customerId);
+    const amountVO = new AmountVO(request.amount);
+    const currencyVO = CurrencyVO.create(request.currency);
+    const statusVO = new PaymentStatusVO('PENDING');
+
+    const order = new OrderEntity({
+      customerId: customerIdVO,
+      creatorId: request.creatorId,
+      amount: amountVO,
+      currency: currencyVO,
+      description: request.description,
+      status: statusVO,
+    });
+
+    await this.orderRepository.save(order);
+
+    return { order };
+  }
+}
