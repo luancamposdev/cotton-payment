8bc495f799b4a5f9b52d99ba2f083f18ca14e328
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-07 15:18:51 -0300

    feat(order): add `UpdateOrderDTO` with validation rules
    - Introduced `UpdateOrderDTO` to define structure and validation for updating orders.

---
+++ apps/api/src/interfaces/order/dto/update-order.dto.ts
@@ -0,0 +1,26 @@
+// update-order.dto.ts
+import { IsString, IsOptional, IsNumber, IsIn } from 'class-validator';
+
+export class UpdateOrderDTO {
+  @IsString()
+  @IsOptional()
+  creatorId?: string;
+
+  @IsNumber()
+  @IsOptional()
+  amount?: number;
+
+  @IsString()
+  @IsIn(['BRL', 'USD', 'EUR'])
+  @IsOptional()
+  currency?: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsIn(['PENDING', 'PAID', 'FAILED', 'CANCELLED'])
+  @IsOptional()
+  status?: string;
+}
