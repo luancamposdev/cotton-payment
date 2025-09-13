6a22c8f5ca517420339bd9593fcb7bdea75b1014
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-13 19:47:03 -0300

    feat(payout): implement `GetPayoutByIdUseCase` to retrieve payout details by ID

---
+++ apps/api/src/application/payout/use-cases/get-payout-by-is.use-case.ts
@@ -0,0 +1,29 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+import { PayoutEntity } from '@core/payout/entities/payout.entity';
+
+import { PayoutRepository } from '@core/payout/repositories/payout.repository';
+import { PayoutIdVO } from '@core/payout/value-objects/payout-id.vo';
+
+export interface GetPayoutByIdRequest {
+  id: string;
+}
+
+export interface GetPayoutByIdResponse {
+  payout: PayoutEntity;
+}
+
+@Injectable()
+export class GetPayoutByIsUseCase {
+  constructor(private readonly payoutRepository: PayoutRepository) {}
+
+  async execute(request: GetPayoutByIdRequest): Promise<GetPayoutByIdResponse> {
+    const payoutId = new PayoutIdVO(request.id);
+
+    const payout = await this.payoutRepository.findById(payoutId);
+
+    if (!payout) throw new NotFoundException('Pagamento não encontrado.');
+
+    return { payout };
+  }
+}
