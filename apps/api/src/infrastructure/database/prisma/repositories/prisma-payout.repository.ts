5b88723e96fecbf30cf9650a576008cc4e85f468
Luan Campos <luancamposdeveloper@gmail.com>
2025-09-14 15:32:59 -0300

    refactor(payout): enhance `PrismaPayoutRepository` with `PrismaService` and extend repository methods

--- apps/api/src/infrastructure/database/prisma/repositories/prisma-payout.repository.ts
+++ apps/api/src/infrastructure/database/prisma/repositories/prisma-payout.repository.ts
@@ -1,14 +1,20 @@
-import { PrismaClient, Prisma } from '@prisma/client';
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PayoutEntity } from '@core/payout/entities/payout.entity';
 import { PayoutMapper } from '@infrastructure/database/prisma/mappers/payout.mapper';
+import { PayoutRepository } from '@core/payout/repositories/payout.repository';
+import { PayoutIdVO } from '@core/payout/value-objects/payout-id.vo';
+import { CreatorPayoutConfigIdVO } from '@core/payout/value-objects/creator-payout-config-id.vo';
+import { PrismaService } from '@infrastructure/database/prisma/prisma.service';
 
-export class PrismaPayoutRepository {
-  constructor(private readonly prisma: PrismaClient) {}
+@Injectable()
+export class PrismaPayoutRepository implements PayoutRepository {
+  constructor(private readonly prismaService: PrismaService) {}
 
-  async save(entity: PayoutEntity): Promise<void> {
+  async create(entity: PayoutEntity): Promise<void> {
     const data = PayoutMapper.toPersistence(entity);
 
-    await this.prisma.payout.create({
+    await this.prismaService.payout.create({
       data: {
         ...data,
         rawPayload: data.rawPayload ?? Prisma.DbNull,
@@ -16,10 +22,10 @@ export class PrismaPayoutRepository {
     });
   }
 
-  async update(entity: PayoutEntity): Promise<void> {
+  async save(entity: PayoutEntity): Promise<void> {
     const data = PayoutMapper.toPersistence(entity);
 
-    await this.prisma.payout.update({
+    await this.prismaService.payout.update({
       where: { id: entity.id.value },
       data: {
         ...data,
@@ -28,8 +34,31 @@ export class PrismaPayoutRepository {
     });
   }
 
-  async findById(id: string): Promise<PayoutEntity | null> {
-    const model = await this.prisma.payout.findUnique({ where: { id } });
-    return model ? PayoutMapper.toEntity(model) : null;
+  async delete(id: PayoutIdVO): Promise<void> {
+    await this.prismaService.payout.delete({
+      where: { id: id.value },
+    });
+  }
+
+  async findById(id: PayoutIdVO): Promise<PayoutEntity | null> {
+    const raw = await this.prismaService.payout.findUnique({
+      where: { id: id.value },
+    });
+    return raw ? PayoutMapper.toEntity(raw) : null;
+  }
+
+  async findByCreatorConfigId(
+    creatorPayoutConfigId: CreatorPayoutConfigIdVO,
+  ): Promise<PayoutEntity[]> {
+    const raws = await this.prismaService.payout.findMany({
+      where: { creatorPayoutConfigId: creatorPayoutConfigId.value },
+    });
+
+    return raws.map((raw) => PayoutMapper.toEntity(raw));
+  }
+
+  async list(): Promise<PayoutEntity[]> {
+    const raws = await this.prismaService.payout.findMany();
+    return raws.map((raw) => PayoutMapper.toEntity(raw));
   }
 }
